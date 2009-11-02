70de1b31f0b17d7b681d34c10f4c81082b9e58f0
Adam James <atj@pulsewidth.org.uk>
2009-11-02 20:54:42 +0000

    Check for additional headers and use config.h

--- src/ssh-askpass-fullscreen.c
+++ src/ssh-askpass-fullscreen.c
@@ -36,10 +36,26 @@
 #define GRAB_TRIES      16
 #define GRAB_WAIT       250 /* milliseconds */   
 
-#include <stdlib.h>
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef HAVE_STDIO_H
 #include <stdio.h>
+#endif
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifdef HAVE_STRING_H
 #include <string.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
 #include <unistd.h>
+#endif
+
 #include <X11/Xlib.h>
 #include <gdk/gdkx.h>
 #include <gtk/gtk.h>
